ca7f6d061a581e8350f7b130c4911931d0626480
David Hartunian <david@musicismath.net>
2015-06-13 14:51:57 -0400

    Finished inputting code

--- muumuu_fsm.erl
+++ muumuu_fsm.erl
@@ -5,9 +5,12 @@
 
 start() ->
     %% Seed PRNG
-    <<A:32, B:32, C:32>> = crypto::rand_bytes(12),
+    <<A:32, B:32, C:32>> = crypto:rand_bytes(12),
     random:seed(A,B,C),
     wait_any_key().
+
+
+%% States and Transitions %%
 
 wait_any_key() ->
     io:get_line("To start, Press Any Key.\n> "),
@@ -47,3 +50,104 @@ wait_for_command() ->
             end
     end,
     wait_for_command().
+
+%% Options and Response Handling %%
+
+option(Prompt) ->
+    show_option(Prompt),
+    Data = io:get_line(""),
+    case iolist_to_binary(Data) of
+        <<"Y", _/binary>> ->
+            yes;
+        <<"y", _/binary>> ->
+            yes;
+        <<"N", _/binary>> ->
+            no;
+        <<"n", _/binary>> ->
+            no;
+        _ ->
+             ambiguous
+    end.
+
+show_option(Str) ->
+     io:format("~s (Y/N)~n> ", [Str]).
+
+wait_cmd(Timeout) ->
+    Parent = self(),
+    Pid = spawn(fun() -> Parent ! io:get_line("") end),
+    receive
+        Data ->
+             Data
+    after Timeout ->
+            exit(Pid, kill),
+            timeout
+    end.
+
+random_option() ->
+    Pos = random:uniform(tuple_size(opts())),
+    {_, Val} = element(Pos, opts()),
+    Val.
+
+match_option(Data) ->
+    case [Vals || {Pattern, Vals} <- tuple_to_list(opts()),
+                  nomatch =/= re:run(Data, Pattern, [caseless])] of
+        [Opt|_] -> Opt;
+        [] -> invalid_opt
+    end.
+
+%% Options/Events
+
+opts() ->
+    {{"(check|core|temp)",
+     {"Check core temperature?",
+      fun core_temperature/0,
+      fun noop/0}},
+    {"(vent|rad|gas)",
+     {"Vent radioactive gas?",
+      fun vent_gas/0,
+      fun no_venting/0}},
+    {"(sound|alert|horn)",
+     {"Sound alertness horn?",
+      fun sound_horn/0,
+      fun noop/0}},
+    {"(calc|duct)",
+     {"Decalcify calcium ducts?",
+      fun noop/0,
+      fun noop/0}}}.
+
+noop() ->
+     ok.
+
+venting_prevents_explosions() ->
+    case option("Venting prevent explosion.") of
+        yes -> blow_crops_away();
+        no -> noop()
+    end.
+
+core_temperature() ->
+    io:format("Core temperature normal.~n").
+
+blow_crops_away() ->
+    io:format("*Gas blows away corn crop*~n").
+
+sound_horn() ->
+    io:format("*horn sounds in the distance*~n").
+
+pressure_too_high() ->
+    io:format("Pressure too high. Tank must be shut down manually.~n").
+
+vent_gas() ->
+    case get(missed) of
+        ?MAX_NO_VENT->
+            pressure_too_high();
+        _ ->
+            put(missed, 0),
+            blow_crops_away()
+    end.
+
+no_venting() ->
+    case get(missed) of
+        undefined ->
+            put(missed, 1);
+        N -> put(missed, N+1)
+    end.
